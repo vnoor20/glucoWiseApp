825a0e4b331e9cedb225521b6df36479ee921b49
Harnoor Kaur <122888307+vnoor20@users.noreply.github.com>
2025-05-24 15:29:10 +0530

    Delete glucoWise.site directory

--- glucoWise.site/README.md
+++
@@ -1,3 +0,0 @@
-# sb1-72dwefiq
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/vnoor20/sb1-72dwefiq)
--- glucoWise.site/src/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://cbcmbvlinobknzpyfnry.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiY21idmxpbm9ia256cHlmbnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxNzcxOTcsImV4cCI6MjA1OTc1MzE5N30._SL5VSc17L6eSFM5t4SZ6UzoCZNRyqrRQWlGr22VXqI';
-export const supabase = createClient(supabaseUrl, supabaseKey);
